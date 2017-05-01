6cd85b9d851b12dc43d9bac537131640ec3930dc
Steph Locke <steph@itsalocke.com>
2017-05-01 20:46:40 +0100

    Add minimal test

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,4 @@
 library(testthat)
-library(Datasaurus)
+library(datasauRus)
 
-test_check("Datasaurus")
+test_check("datasauRus")
---
+++ tests/testthat/test-rawfiles.R
@@ -0,0 +1,7 @@
+context("Raw files")
+
+test_that("extdata exists", {
+  files<-list.files(system.file("extdata",package="datasauRus"))
+  expect_equal(length(files),9)
+
+})
