b147f08f8f9c940a5cf5a905bc45c73f37d6ea90
Lucy <ld.mcgowan@vanderbilt.edu>
2017-05-02 08:55:50 -0500

    make reference a bit prettier 💅🏼

--- R/Datasaurus-package.r
+++ R/Datasaurus-package.r
@@ -8,97 +8,106 @@ NULL
 #'@docType data
 #'@name box_plots
 #'@usage data(box_plots)
-#'@references Justin Matejka, George Fitzmaurice. (2017) Same Stats,
-#'Different Graphs: Generating Datasets with Varied Appearance and
-#'Identical Statistics through Simulated Annealing.
-#'CHI 2017 Conference proceedings: ACM SIGCHI Conference on Human
-#'Factors in Computing Systems.
+#'@references Matejka, J., & Fitzmaurice, G. (2017).
+#'Same Stats, Different Graphs: Generating Datasets with
+#'Varied Appearance and Identical Statistics through Simulated
+#'Annealing. _CHI 2017 Conference proceedings: ACM SIGCHI
+#'Conference on Human Factors in Computing Systems._
+#' Retrieved from [https://www.autodeskresearch.com/publications/samestats](https://www.autodeskresearch.com/publications/samestats).
 "box_plots"
 
 #'Datasaurus Dozen (wide) data
 #'@docType data
 #'@name datasaurus_dozen_wide
 #'@usage data(datasaurus_dozen_wide)
-#'@references Justin Matejka, George Fitzmaurice. (2017) Same Stats,
-#'Different Graphs: Generating Datasets with Varied Appearance and
-#'Identical Statistics through Simulated Annealing.
-#'CHI 2017 Conference proceedings: ACM SIGCHI Conference on Human
-#'Factors in Computing Systems.
+#'@references Matejka, J., & Fitzmaurice, G. (2017).
+#'Same Stats, Different Graphs: Generating Datasets with
+#'Varied Appearance and Identical Statistics through Simulated
+#'Annealing. _CHI 2017 Conference proceedings: ACM SIGCHI
+#'Conference on Human Factors in Computing Systems._
+#' Retrieved from [https://www.autodeskresearch.com/publications/samestats](https://www.autodeskresearch.com/publications/samestats).
 "datasaurus_dozen_wide"
 
 #'Datasaurus Dozen data
 #'@docType data
 #'@name datasaurus_dozen
 #'@usage data(datasaurus_dozen)
-#'@references Justin Matejka, George Fitzmaurice. (2017) Same Stats,
-#'Different Graphs: Generating Datasets with Varied Appearance and
-#'Identical Statistics through Simulated Annealing.
-#'CHI 2017 Conference proceedings: ACM SIGCHI Conference on Human
-#'Factors in Computing Systems.
+#'@references Matejka, J., & Fitzmaurice, G. (2017).
+#'Same Stats, Different Graphs: Generating Datasets with
+#'Varied Appearance and Identical Statistics through Simulated
+#'Annealing. _CHI 2017 Conference proceedings: ACM SIGCHI
+#'Conference on Human Factors in Computing Systems._
+#' Retrieved from [https://www.autodeskresearch.com/publications/samestats](https://www.autodeskresearch.com/publications/samestats).
 "datasaurus_dozen"
 
 #'Simpsons Paradox (wide) data
 #'@docType data
 #'@name simpsons_paradox_wide
 #'@usage data(simpsons_paradox_wide)
-#'@references Justin Matejka, George Fitzmaurice. (2017) Same Stats,
-#'Different Graphs: Generating Datasets with Varied Appearance and
-#'Identical Statistics through Simulated Annealing.
-#'CHI 2017 Conference proceedings: ACM SIGCHI Conference on Human
-#'Factors in Computing Systems.
+#'@references Matejka, J., & Fitzmaurice, G. (2017).
+#'Same Stats, Different Graphs: Generating Datasets with
+#'Varied Appearance and Identical Statistics through Simulated
+#'Annealing. _CHI 2017 Conference proceedings: ACM SIGCHI
+#'Conference on Human Factors in Computing Systems._
+#' Retrieved from [https://www.autodeskresearch.com/publications/samestats](https://www.autodeskresearch.com/publications/samestats).
 "simpsons_paradox_wide"
 
 #'Simpsons Paradox data
 #'@docType data
 #'@name simpsons_paradox
 #'@usage data(simpsons_paradox)
-#'@references Justin Matejka, George Fitzmaurice. (2017) Same Stats,
-#'Different Graphs: Generating Datasets with Varied Appearance and
-#'Identical Statistics through Simulated Annealing.
-#'CHI 2017 Conference proceedings: ACM SIGCHI Conference on Human
-#'Factors in Computing Systems.
+#'@references Matejka, J., & Fitzmaurice, G. (2017).
+#'Same Stats, Different Graphs: Generating Datasets with
+#'Varied Appearance and Identical Statistics through Simulated
+#'Annealing. _CHI 2017 Conference proceedings: ACM SIGCHI
+#'Conference on Human Factors in Computing Systems._
+#' Retrieved from [https://www.autodeskresearch.com/publications/samestats](https://www.autodeskresearch.com/publications/samestats).
 "simpsons_paradox"
 
 #'Twelve From Slant Alternate (long) data
 #'@docType data
 #'@name twelve_from_slant_alternate_long
 #'@usage data(twelve_from_slant_alternate_long)
-#'@references Justin Matejka, George Fitzmaurice. (2017) Same Stats,
-#'Different Graphs: Generating Datasets with Varied Appearance and
-#'Identical Statistics through Simulated Annealing.
-#'CHI 2017 Conference proceedings: ACM SIGCHI Conference on Human
-#'Factors in Computing Systems.
+#'@references Matejka, J., & Fitzmaurice, G. (2017).
+#'Same Stats, Different Graphs: Generating Datasets with
+#'Varied Appearance and Identical Statistics through Simulated
+#'Annealing. _CHI 2017 Conference proceedings: ACM SIGCHI
+#'Conference on Human Factors in Computing Systems._
+#' Retrieved from [https://www.autodeskresearch.com/publications/samestats](https://www.autodeskresearch.com/publications/samestats).
 "twelve_from_slant_alternate_long"
 
 #'Twelve From Slant Alternate (wide) data
 #'@docType data
 #'@name twelve_from_slant_alternate_wide
 #'@usage data(twelve_from_slant_alternate_wide)
-#'@references Justin Matejka, George Fitzmaurice. (2017) Same Stats,
-#'Different Graphs: Generating Datasets with Varied Appearance and
-#'Identical Statistics through Simulated Annealing.
-#'CHI 2017 Conference proceedings: ACM SIGCHI Conference on Human
-#'Factors in Computing Systems.
+#'@references Matejka, J., & Fitzmaurice, G. (2017).
+#'Same Stats, Different Graphs: Generating Datasets with
+#'Varied Appearance and Identical Statistics through Simulated
+#'Annealing. _CHI 2017 Conference proceedings: ACM SIGCHI
+#'Conference on Human Factors in Computing Systems._
+#' Retrieved from [https://www.autodeskresearch.com/publications/samestats](https://www.autodeskresearch.com/publications/samestats).
 "twelve_from_slant_alternate_wide"
 
 #'Twelve From Slant (long) data
 #'@docType data
 #'@name twelve_from_slant_long
 #'@usage data(twelve_from_slant_long)
-#'@references Justin Matejka, George Fitzmaurice. (2017) Same Stats,
-#'Different Graphs: Generating Datasets with Varied Appearance and
-#'Identical Statistics through Simulated Annealing.
-#'CHI 2017 Conference proceedings: ACM SIGCHI Conference on Human
-#'Factors in Computing Systems.
+#'@references Matejka, J., & Fitzmaurice, G. (2017).
+#'Same Stats, Different Graphs: Generating Datasets with
+#'Varied Appearance and Identical Statistics through Simulated
+#'Annealing. _CHI 2017 Conference proceedings: ACM SIGCHI
+#'Conference on Human Factors in Computing Systems._
+#' Retrieved from [https://www.autodeskresearch.com/publications/samestats](https://www.autodeskresearch.com/publications/samestats).
 "twelve_from_slant_long"
 
 #'Twelve From Slant (wide) data
 #'@docType data
 #'@name twelve_from_slant_wide
 #'@usage data(twelve_from_slant_wide)
-#'@references Justin Matejka, George Fitzmaurice. (2017) Same Stats,
-#'Different Graphs: Generating Datasets with Varied Appearance and
-#'Identical Statistics through Simulated Annealing.
-#'CHI 2017 Conference proceedings: ACM SIGCHI Conference on Human
-#'Factors in Computing Systems.
+#'@references Matejka, J., & Fitzmaurice, G. (2017).
+#'Same Stats, Different Graphs: Generating Datasets with
+#'Varied Appearance and Identical Statistics through Simulated
+#'Annealing. _CHI 2017 Conference proceedings: ACM SIGCHI
+#'Conference on Human Factors in Computing Systems._
+#' Retrieved from [https://www.autodeskresearch.com/publications/samestats](https://www.autodeskresearch.com/publications/samestats).
 "twelve_from_slant_wide"
