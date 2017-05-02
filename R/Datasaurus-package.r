330857b3dae031120039f4e513c91cbb757d0610
Lucy <ld.mcgowan@vanderbilt.edu>
2017-05-02 08:49:09 -0500

    add some more to docs (usage, refs, ect) 🌻

--- R/Datasaurus-package.r
+++ R/Datasaurus-package.r
@@ -5,28 +5,100 @@
 NULL
 
 #'Box Plots data
+#'@docType data
+#'@name box_plots
+#'@usage data(box_plots)
+#'@references Justin Matejka, George Fitzmaurice. (2017) Same Stats,
+#'Different Graphs: Generating Datasets with Varied Appearance and
+#'Identical Statistics through Simulated Annealing.
+#'CHI 2017 Conference proceedings: ACM SIGCHI Conference on Human
+#'Factors in Computing Systems.
 "box_plots"
 
 #'Datasaurus Dozen (wide) data
+#'@docType data
+#'@name datasaurus_dozen_wide
+#'@usage data(datasaurus_dozen_wide)
+#'@references Justin Matejka, George Fitzmaurice. (2017) Same Stats,
+#'Different Graphs: Generating Datasets with Varied Appearance and
+#'Identical Statistics through Simulated Annealing.
+#'CHI 2017 Conference proceedings: ACM SIGCHI Conference on Human
+#'Factors in Computing Systems.
 "datasaurus_dozen_wide"
 
 #'Datasaurus Dozen data
+#'@docType data
+#'@name datasaurus_dozen
+#'@usage data(datasaurus_dozen)
+#'@references Justin Matejka, George Fitzmaurice. (2017) Same Stats,
+#'Different Graphs: Generating Datasets with Varied Appearance and
+#'Identical Statistics through Simulated Annealing.
+#'CHI 2017 Conference proceedings: ACM SIGCHI Conference on Human
+#'Factors in Computing Systems.
 "datasaurus_dozen"
 
 #'Simpsons Paradox (wide) data
+#'@docType data
+#'@name simpsons_paradox_wide
+#'@usage data(simpsons_paradox_wide)
+#'@references Justin Matejka, George Fitzmaurice. (2017) Same Stats,
+#'Different Graphs: Generating Datasets with Varied Appearance and
+#'Identical Statistics through Simulated Annealing.
+#'CHI 2017 Conference proceedings: ACM SIGCHI Conference on Human
+#'Factors in Computing Systems.
 "simpsons_paradox_wide"
 
 #'Simpsons Paradox data
+#'@docType data
+#'@name simpsons_paradox
+#'@usage data(simpsons_paradox)
+#'@references Justin Matejka, George Fitzmaurice. (2017) Same Stats,
+#'Different Graphs: Generating Datasets with Varied Appearance and
+#'Identical Statistics through Simulated Annealing.
+#'CHI 2017 Conference proceedings: ACM SIGCHI Conference on Human
+#'Factors in Computing Systems.
 "simpsons_paradox"
 
 #'Twelve From Slant Alternate (long) data
+#'@docType data
+#'@name twelve_from_slant_alternate_long
+#'@usage data(twelve_from_slant_alternate_long)
+#'@references Justin Matejka, George Fitzmaurice. (2017) Same Stats,
+#'Different Graphs: Generating Datasets with Varied Appearance and
+#'Identical Statistics through Simulated Annealing.
+#'CHI 2017 Conference proceedings: ACM SIGCHI Conference on Human
+#'Factors in Computing Systems.
 "twelve_from_slant_alternate_long"
 
 #'Twelve From Slant Alternate (wide) data
+#'@docType data
+#'@name twelve_from_slant_alternate_wide
+#'@usage data(twelve_from_slant_alternate_wide)
+#'@references Justin Matejka, George Fitzmaurice. (2017) Same Stats,
+#'Different Graphs: Generating Datasets with Varied Appearance and
+#'Identical Statistics through Simulated Annealing.
+#'CHI 2017 Conference proceedings: ACM SIGCHI Conference on Human
+#'Factors in Computing Systems.
 "twelve_from_slant_alternate_wide"
 
 #'Twelve From Slant (long) data
+#'@docType data
+#'@name twelve_from_slant_long
+#'@usage data(twelve_from_slant_long)
+#'@references Justin Matejka, George Fitzmaurice. (2017) Same Stats,
+#'Different Graphs: Generating Datasets with Varied Appearance and
+#'Identical Statistics through Simulated Annealing.
+#'CHI 2017 Conference proceedings: ACM SIGCHI Conference on Human
+#'Factors in Computing Systems.
 "twelve_from_slant_long"
 
 #'Twelve From Slant (wide) data
+#'@docType data
+#'@name twelve_from_slant_wide
+#'@usage data(twelve_from_slant_wide)
+#'@references Justin Matejka, George Fitzmaurice. (2017) Same Stats,
+#'Different Graphs: Generating Datasets with Varied Appearance and
+#'Identical Statistics through Simulated Annealing.
+#'CHI 2017 Conference proceedings: ACM SIGCHI Conference on Human
+#'Factors in Computing Systems.
 "twelve_from_slant_wide"
