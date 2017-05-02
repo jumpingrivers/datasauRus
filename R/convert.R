4e9f2cc7405325589fa32479b43982e2243d2d30
Lucy <ld.mcgowan@vanderbilt.edu>
2017-05-01 22:12:42 -0500

    fix wide data 💃🏽

--- R/convert.R
+++ R/convert.R
@@ -6,7 +6,15 @@ purrr::walk(files, function(x){
 
   #make it slither
   nm <-  gsub("^_+", "", tolower(gsub("([A-Z])", "_\\1", nm)))
-  .dat <- read.table(x)
+
+  if (grepl("wide",nm)){
+  header1 <- scan(x, nlines = 1, what = character())
+  header2 <- scan(x, skip = 1, nlines = 1, what = character())
+  .dat <- read.table(x, skip = 2, header = FALSE)
+  names(.dat) <- paste0(header1,"_", header2)
+  } else {
+    .dat <- read.table(x, header = TRUE)
+  }
   assign(nm, .dat)
   save(list = nm, file = paste0("data/",nm,".rda"))
 })
