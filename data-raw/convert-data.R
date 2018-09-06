5e791778b5c6dbfac644ec8ddfda1ec3b31dfc69
Maëlle Salmon <maelle.salmon@yahoo.se>
2018-09-06 09:01:33 +0200

    Update Box Plot Examples (#27)
    add updated boxplots file by @jmatejka

--- data-raw/convert-data.R
+++ data-raw/convert-data.R
@@ -1,4 +1,4 @@
-#here is how I converted, we can delete this
+#here is how I converted
 files =  list.files(path = "./inst/extdata",pattern="*.tsv", full.names = TRUE)
 
 purrr::walk(files, function(x){
@@ -7,7 +7,7 @@ purrr::walk(files, function(x){
 
   #make it slither
   nm <-  gsub("^_+", "", tolower(gsub("([A-Z])", "_\\1", nm)))
-
+  nm <- gsub("\\_\\_", "\\_", nm)
   if (grepl("wide",nm)){
     header1 <- scan(x, nlines = 1, what = character())
     header1 <- paste(header1,c("x","y"),sep="_")
@@ -20,5 +20,7 @@ purrr::walk(files, function(x){
 })
 
 # remove box_plots rownames
-box_plots <- box_plots[,-1]
+box_plots <- readr::read_tsv("./inst/extdata/BoxPlots.tsv")[,2:6]
 save(box_plots, file = "data/box_plots.rda")
+
+
